87958d33c4edbe76b19a74063c29b8d876f32255
Sarah Thomas <s4r4hthomas@gmail.com>
2020-01-12 17:27:10 -0500

    Added destroy route

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
-//Definitions
+//~~~~~~~~~~~~~~~~~~~//
+//    Definitions    //
+//~~~~~~~~~~~~~~~~~~~//
+
 const   express        = require("express"),
         app            = express(),
         bodyParser     = require("body-parser"),
         mongoose       = require("mongoose"),
         methodOverride = require("method-override");
+
+//~~~~~~~~~~~~~~~~~~//
+//    App Config    //
+//~~~~~~~~~~~~~~~~~~//
 
 //Fix mongoose deprecation warnings
 mongoose.set('useNewUrlParser', true);
@@ -11,7 +18,6 @@ mongoose.set('useFindAndModify', false);
 mongoose.set('useCreateIndex', true);
 mongoose.set('useUnifiedTopology', true);
 
-//App Config
 mongoose.connect("mongodb://localhost/blog_app"); //connect JS to MongoDB
 app.set("view engine", "ejs"); //Tells express that /views are ejs files
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -27,13 +33,17 @@ const blogSchema = new mongoose.Schema({
 });
 const Post = mongoose.model("Post", blogSchema);
 
-//Home page
+//~~~~~~~~~~~~~~~~~~//
+//  RESTful Routes  //
+//~~~~~~~~~~~~~~~~~~//
+
+//Landing page
 app.get("/", function(req, res){
     res.redirect("/posts");
-})
+});
 
-//RESTful Routes
-app.get("/posts", function(req, res){ //index - show all blog posts
+//INDEX - Show all blog posts
+app.get("/posts", function(req, res){
     Post.find({}, function(err, allPosts){
         if (err){
             console.log(err);
@@ -43,11 +53,13 @@ app.get("/posts", function(req, res){ //index - show all blog posts
     });   
 });
 
-app.get("/posts/new", function(req, res){ //new - show form to add a new blog post
+//NEW - Show form to add a new blog post
+app.get("/posts/new", function(req, res){
     res.render("new");
 });
 
-app.post("/posts", function(req, res){ //create - create a new blog post and redirect to index
+//CREATE - Create a new blog post and redirect to index
+app.post("/posts", function(req, res){
     //create a new blog post
     Post.create(req.body.post, function(err, newPost){
         if (err){
@@ -58,7 +70,8 @@ app.post("/posts", function(req, res){ //create - create a new blog post and red
     });
 });
 
-app.get("/posts/:id", function(req, res){ //show - shows all info about a specific blog post
+//SHOW - Shows all information about a specific blog post
+app.get("/posts/:id", function(req, res){
     //get the specific id of the post
     Post.findById(req.params.id, function(err, foundPost){
         if (err){
@@ -69,7 +82,8 @@ app.get("/posts/:id", function(req, res){ //show - shows all info about a specif
     });
 });
 
-app.get("/posts/:id/edit", function(req, res){ //edit - show form to edit an existing blog post
+//EDIT - Show form to edit an existing blog post
+app.get("/posts/:id/edit", function(req, res){
     Post.findById(req.params.id, function(err, foundPost){
         if (err){
             res.redirect("/posts");
@@ -79,7 +93,8 @@ app.get("/posts/:id/edit", function(req, res){ //edit - show form to edit an exi
     });
 });
 
-app.put("/posts/:id", function(req, res){ //update - update the existing blog post and redirect to the show page
+//UPDATE - Update an existing blog post and redirect to the show page
+app.put("/posts/:id", function(req, res){
     Post.findByIdAndUpdate(req.params.id, req.body.post, function(err, updatedPost){
         if (err){
             res.redirect("/posts");
@@ -89,7 +104,21 @@ app.put("/posts/:id", function(req, res){ //update - update the existing blog po
     });
 });
 
-//Start server
+//DESTROY - Delete an existing blog post and redirect to the index page
+app.delete("/posts/:id", function(req, res){
+    Post.findByIdAndDelete(req.params.id, function(err){
+        if (err){
+            res.redirect("/posts/");
+        } else{
+            res.redirect("/posts");
+        }
+    });
+});
+
+//~~~~~~~~~~~~~~~~~~//
+//   Start Server   //
+//~~~~~~~~~~~~~~~~~~//
+
 app.listen(3000, function(){
     console.log("Server started on port 3000");
 });
